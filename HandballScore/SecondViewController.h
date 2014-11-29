fbe46639b1b8c7ed16d4a60e52361b2436aaf402
Penspinner <stevenliao@Stevens-MacBook-Pro.local>
2014-11-29 00:02:05 -0500

    Initial Commit

---
+++ HandballScore/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  HandballScore
+//
+//  Created by Steven Liao on 11/29/14.
+//  Copyright (c) 2014 Steven Liao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ HandballScoreTests/HandballScoreTests.m
@@ -0,0 +1,40 @@
+//
+//  HandballScoreTests.m
+//  HandballScoreTests
+//
+//  Created by Steven Liao on 11/29/14.
+//  Copyright (c) 2014 Steven Liao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface HandballScoreTests : XCTestCase
+
+@end
+
+@implementation HandballScoreTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
